10abd89e64c3d81455b412c36535d38d9129bac9
Alex McRitchie <amcritchie@gmail.com>
2014-08-11 10:57:35 -0600

    Add gif page.

--- app/controllers/gifs_controller.rb
+++ app/controllers/gifs_controller.rb
@@ -6,4 +6,24 @@ class GifsController < ApplicationController
   def new
     @gifs = Gif.new
   end
+
+  def create
+
+    @gifs = Gif.new
+    @gifs.name = params[:gifs][:name]
+    @gifs.name = params[:gifs][:url]
+
+    if @gifs.valid?
+      if @gifs.save
+        # flash[:success] = "Task List was created."
+        redirect_to "/"
+      else
+        @gifs
+        render :new
+      end
+    else
+      # flash[:error] = "Your tasks list could not be created."
+      render :new
+    end
+  end
 end
